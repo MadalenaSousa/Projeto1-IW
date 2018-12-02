28c30e2085844ef64dff58310811678b194a3069
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-12-02 21:47:36 +0000

    ...

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 let r, y, b;
+let nTriRed, nTriYellow, nTriBlue, nTriAll;
+let rand;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -12,13 +14,20 @@ function setup() {
     r = 50;
     y = 700;
     b = 200;
+
+    rand = 0;
 }
 
 function draw() {
     background(255);
     canvas.position(0,window.scrollY);
 
-    esticar();
+    if (rand === 0) {
+        triangulosEncolhem();
+    } else if (rand === 1) {
+        esticar();
+    }
+
 }
 
 function esticar() {
@@ -77,4 +86,73 @@ function esticar() {
 
     stroke(0, 0, 255);
     triangle(windowWidth/2, 0, windowWidth, 0, windowWidth, b);
+}
+
+function triangulosEncolhem() {
+    noFill();
+    strokeWeight(8);
+
+    if ((mouseX > 50 && mouseX < 450) && (mouseY > 550 && mouseY < 550 + 180)) {
+        if (nTriRed < 5) {
+            nTriRed++;
+        }
+    } else {
+        if (nTriRed > 0) {
+            nTriRed--;
+        }
+    }
+
+    if ((mouseX > 450 && mouseX < 750) && (mouseY > 450 && mouseY < 450 + 150)) {
+        if (nTriBlue < 5) {
+            nTriBlue++;
+        }
+    } else {
+        if (nTriBlue > 0) {
+            nTriBlue--;
+        }
+    }
+
+    if ((mouseX > 350 && mouseX < 700) && (mouseY > 800 && mouseY < 800 + 150)) {
+        if (nTriYellow < 5) {
+            nTriYellow++;
+        }
+    } else {
+        if (nTriYellow > 0) {
+            nTriYellow--;
+        }
+    }
+
+    if ((mouseX > 50 && mouseX < 600) && (mouseY > 50 && mouseY < 350)) {
+        if (nTriAll < 5) {
+            nTriAll++;
+        }
+    } else {
+        if (nTriAll > 0) {
+            nTriAll--;
+        }
+    }
+
+    for (let i=0; i<nTriRed; i++) {
+        stroke(255, 0, 0);
+        triangle((i*30), ((i*2+1)*30), (i*30), 700 - ((i*2+1)*30), 300 - (i*30), 350);
+    }
+
+    for (let i=0; i<nTriYellow; i++) {
+        stroke(255, 255, 0);
+        triangle(50 + ((i*2+1)*30), 1000 - (i*30), 400, 200 + ((i*3+1)*30), 700 - ((i*2+1)*30), 1000 - (i*30));
+    }
+
+    for (let i=0; i<nTriBlue; i++) {
+        stroke(0, 0, 255);
+        triangle(400 + (i*60), (i*40), 800 - (i*30), (i*40), 800 - (i*25), 900 - ((i*4+1)*30));
+    }
+
+    for (let i=0; i<nTriAll; i++) {
+        stroke(255, 0, 0);
+        triangle((i*30), ((i*2+1)*30), (i*30), 700 - ((i*2+1)*30), 300 - (i*30), 350);
+        stroke(255, 255, 0);
+        triangle(50 + ((i*2+1)*30), 1000 - (i*30), 400, 200 + ((i*3+1)*30), 700 - ((i*2+1)*30), 1000 - (i*30));
+        stroke(0, 0, 255);
+        triangle(400 + (i*60), (i*40), 800 - (i*30), (i*40), 800 - (i*25), 900 - ((i*4+1)*30));
+    }
 }
