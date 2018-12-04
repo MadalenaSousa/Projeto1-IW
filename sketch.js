e778c553288247afe8e304b5c33fbc5fdcd5714f
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-12-04 11:06:38 +0000

    last updates

--- sketch.js
+++ sketch.js
@@ -20,69 +20,37 @@ function setup() {
     logo = select('.logo');
     console.log(logo);
 
-    rand = 1;
+    rand = 0;
 }
 
 function draw() {
-    background(255);
     canvas.position(0,window.scrollY);
 
     if (rand === 0) {
         logo.mouseOver(triangulosEncolhem);
+        logo
     } else if (rand === 1) {
-        logo.mouseOver(esticar);
+        esticar();
     }
-
 }
 
 function esticar() {
-    console.log("entrou");
+    console.log("entrou Esticar");
     noFill();
     strokeWeight(10);
 
-    if ((mouseX > 50 && mouseX < 450) && (mouseY > 550 && mouseY < 550 + 180)) {
-        if (r < 350) {
-            r = r + 5;
-        }
-    } else {
-        if (r > 50) {
-            r = r - 5;
-        }
+    if (r < 350) {
+        r = r + 5;
     }
 
-    if ((mouseX > 450 && mouseX < 750) && (mouseY > 450 && mouseY < 450 + 150)) {
-        if (b < 900) {
-            b = b + 5;
-        }
-    } else {
-        if (b > 200) {
-            b = b - 5;
-        }
+    if (b < 900) {
+        b = b + 5;
     }
 
-    if ((mouseX > 350 && mouseX < 700) && (mouseY > 800 && mouseY < 800 + 150)) {
-        if (y > 200) {
-            y = y - 5;
-        }
-    } else {
-        if (y < 700) {
-            y = y + 5;
-        }
+    if (y > 200) {
+        y = y - 5;
     }
 
-    if ((mouseX > 50 && mouseX < 600) && (mouseY > 50 && mouseY < 350)) {
-        if (r < 350 && b < 900 && y < 200) {
-            r = r + 5;
-            b = b + 5;
-            b = b + 5;
-        }
-    } else {
-        if (r > 50 && b > 200 && y < 200) {
-            r = r - 5;
-            b = b - 5;
-            b = b - 5;
-        }
-    }
 
     stroke(255, 0, 0);
     triangle(0, 0, 0, 700, r, 350);
@@ -94,19 +62,21 @@ function esticar() {
     triangle(windowWidth/2, 0, windowWidth, 0, windowWidth, b);
 }
 
+function acao() {
+
+}
+
 function triangulosEncolhem() {
-    console.log("entrou");
+    console.log("entrou triEncolhem");
     noFill();
     strokeWeight(8);
 
-    if ((mouseX > 0 && mouseX < 600) && (mouseY > 0 && mouseY < 600)) {
-        if (nTriAll < 6) {
-            nTriAll++;
-        }
-    } else {
-        if (nTriAll > 0) {
-            nTriAll--;
-        }
+    if (nTriAll < 5) {
+        nTriAll++;
+    }
+
+    if (nTriAll > 0) {
+        nTriAll--;
     }
 
     for (let i=0; i<nTriAll; i++) {
@@ -118,3 +88,4 @@ function triangulosEncolhem() {
         triangle(windowWidth/2 + (i*120), (i*40), windowWidth - (i*45), (i*40), windowWidth - (i*40), 900 - ((i*4+1)*30));
     }
 }
+
