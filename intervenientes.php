c72eb29bd1a8cc4657c627b254d4a70cd4ebaf6c
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2019-01-19 17:51:40 +0000

    ...

--- intervenientes.php
+++ intervenientes.php
@@ -17,6 +17,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Jockey+One" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:700" rel="stylesheet">
 </head>
 
   <body>
@@ -35,60 +36,36 @@
             </div>
             <div class="col-6">
                 <h1 class="jockey">Thomas Castro</h1>
-                <h2>"Tecnologia ao Nosso Serviço"</h2>
-                <div>
-                    Thomas Castro founded LUST  with Jeroen Barendse, and Dimitri Nieuwenhuizen, a multidisciplinary graphic
-                    design practice established in 1996 by , based in The Hague, Netherlands. LUST is deeply interested in
-                    exploring new pathways for design at the cutting edge where new media and information technologies,
-                    architecture and urban systems and graphic design overlap.
-
-                    This fascination led to establishing LUSTlab in the summer of 2010. LUSTlab is more than a new form of
-                    Research & Development. LUSTlab goes further than observing, inventing and producing, by means of forming
-                    a platform where knowledge, issues and ideologies can be shared.
-
-                    LUSTlab researches, generates hypotheses and makes unstable media stable again. The future of digital
-                    media lies in the design of its use. Humanizing the unhuman, bringing the internet down to earth and finding
-                    the missing link between the digital and the physical. The outcomes vary from (strategic) visions to new
-                    communication tools, man-machine installations and physical products using digital content.
-                </div>
+                <h2>Tecnologia ao Nosso Serviço</h2>
+                <h2>11 de Março</h2>
+                <h2>Departamento de Engenharia Informática</h2>
+                <h5>Mais Informação</h5>
             </div>
 
             <div class="col"></div>
         </div>
 
-        <div class="row mario-desktop">
+        <div class="row">
             <div class="col"></div>
+            <div class="col-10">
+                Thomas Castro founded LUST  with Jeroen Barendse, and Dimitri Nieuwenhuizen, a multidisciplinary graphic
+                design practice established in 1996 by , based in The Hague, Netherlands. LUST is deeply interested in
+                exploring new pathways for design at the cutting edge where new media and information technologies,
+                architecture and urban systems and graphic design overlap.
 
-            <div class="col-6">
-                <h1 class="jockey">Mario Feliciano</h1>
-                <h2>"Workshop de Tipografia"</h2>
-                <div>
-                    Born 1969) studied graphic design at IADE (Lisbon) until 1993. Before finishing his degree he started
-                    to work as a graphic designer at Surf Portugal magazine. In 1994 he founded his design studio (Secretonix)
-                    that produced a wide range of design projects from editorial to corporate design. After having commissioned
-                    one typeface for Adobe (called Strumpf) and after publishing some of his early work in other foundries,
-                    in 2001 he creates the Feliciano Type Foundry and starts to publish his own designs and creating custom
-                    typefaces for clients around the world.
+                This fascination led to establishing LUSTlab in the summer of 2010. LUSTlab is more than a new form of
+                Research & Development. LUSTlab goes further than observing, inventing and producing, by means of forming
+                a platform where knowledge, issues and ideologies can be shared.
 
-                    He is also a member of AGI (Alliance Graphique Internationale) since 2009. He is the author of several
-                    custom typefaces, such as: Expresso (for the Portuguese weekly newspaper Expresso), Sueca (for the Swedish
-                    newspaper Svenska Dagbladet), Majerit (for the Spanish newspaper El País) and BesSans for Banco Espírito Santo.
-                    His most popular typeface is Flama that is used as a customised version on the Portuguese Passport and Citizen
-                    Card along with his other typeface Merlo. In the recent years Mário has been working on expanding FTF's library
-                    and giving typographic consultancy.
-                </div>
+                LUSTlab researches, generates hypotheses and makes unstable media stable again. The future of digital
+                media lies in the design of its use. Humanizing the unhuman, bringing the internet down to earth and finding
+                the missing link between the digital and the physical. The outcomes vary from (strategic) visions to new
+                communication tools, man-machine installations and physical products using digital content.
             </div>
-
-            <div class="col-4">
-                <div class="pic">
-                    <img src="imagens/mariofeliciano.jpg" alt="Mario Feliciano">
-                </div>
-            </div>
-
             <div class="col"></div>
         </div>
 
-        <div class="row mario-mobile">
+        <div class="row">
             <div class="col"></div>
 
             <div class="col-4">
@@ -99,24 +76,32 @@
 
             <div class="col-6">
                 <h1 class="jockey">Mario Feliciano</h1>
-                <h2>"Workshop de Tipografia"</h2>
-                <div>
-                    Born 1969) studied graphic design at IADE (Lisbon) until 1993. Before finishing his degree he started
-                    to work as a graphic designer at Surf Portugal magazine. In 1994 he founded his design studio (Secretonix)
-                    that produced a wide range of design projects from editorial to corporate design. After having commissioned
-                    one typeface for Adobe (called Strumpf) and after publishing some of his early work in other foundries,
-                    in 2001 he creates the Feliciano Type Foundry and starts to publish his own designs and creating custom
-                    typefaces for clients around the world.
-
-                    He is also a member of AGI (Alliance Graphique Internationale) since 2009. He is the author of several
-                    custom typefaces, such as: Expresso (for the Portuguese weekly newspaper Expresso), Sueca (for the Swedish
-                    newspaper Svenska Dagbladet), Majerit (for the Spanish newspaper El País) and BesSans for Banco Espírito Santo.
-                    His most popular typeface is Flama that is used as a customised version on the Portuguese Passport and Citizen
-                    Card along with his other typeface Merlo. In the recent years Mário has been working on expanding FTF's library
-                    and giving typographic consultancy.
-                </div>
+                <h2>Workshop de Tipografia</h2>
+                <h2>12 de Março</h2>
+                <h2>Departamento de Arquitetura</h2>
+                <h5>Mais Informação</h5>
             </div>
 
+            <div class="col"></div>
+        </div>
+
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-10">
+                Born 1969) studied graphic design at IADE (Lisbon) until 1993. Before finishing his degree he started
+                to work as a graphic designer at Surf Portugal magazine. In 1994 he founded his design studio (Secretonix)
+                that produced a wide range of design projects from editorial to corporate design. After having commissioned
+                one typeface for Adobe (called Strumpf) and after publishing some of his early work in other foundries,
+                in 2001 he creates the Feliciano Type Foundry and starts to publish his own designs and creating custom
+                typefaces for clients around the world.
+
+                He is also a member of AGI (Alliance Graphique Internationale) since 2009. He is the author of several
+                custom typefaces, such as: Expresso (for the Portuguese weekly newspaper Expresso), Sueca (for the Swedish
+                newspaper Svenska Dagbladet), Majerit (for the Spanish newspaper El País) and BesSans for Banco Espírito Santo.
+                His most popular typeface is Flama that is used as a customised version on the Portuguese Passport and Citizen
+                Card along with his other typeface Merlo. In the recent years Mário has been working on expanding FTF's library
+                and giving typographic consultancy.
+            </div>
             <div class="col"></div>
         </div>
 
@@ -131,7 +116,18 @@
 
             <div class="col-6">
                 <h1 class="jockey">Luna Maurer</h1>
-                <h2>"Design de Interação"</h2>
+                <h2>Design de Interação</h2>
+                <h2>13 de Março</h2>
+                <h2>Laboratório Chimico</h2>
+                <h5>Mais Informação</h5>
+            </div>
+
+            <div class="col"></div>
+        </div>
+
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-10">
                 Luna runs the Amsterdam-based design studio Moniker with Roel Wouters. (They founded it in 2012 with
                 Jonathan Puckey.) Moniker “explores the characteristics of technology, how people use it, and how it
                 influences people’s daily lives. "They’ve taught media courses at the Gerrit Rietveld Academy, the Sandberg
@@ -144,7 +140,6 @@
                 Here, Maurer discusses what it means to break down complicated ideas, the importance of having a guiding
                 manifesto, and the difference (or lack thereof) between an artist and a designer.
             </div>
-
             <div class="col"></div>
         </div>
     </main>
